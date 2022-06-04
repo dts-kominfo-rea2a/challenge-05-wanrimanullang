1a72362f49626cf7ee35e54b5c6312efb7799c7d
wanri parasian simanullang <wanriparasians@gmail.com>
2022-06-04 22:26:15 +0700

    challange-done

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter =  (list,sortFunc) => {
+  sortList = sortFunc(list)
+  let data = []
+  for( var i = 0; i < names.length; i++){
+    data.push(`${i+1}. ${names[i]}`)
+  }
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (list) => {
+  let data = list.sort()
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (list) => {
+  let data = list.sort().reverse()
+  return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
